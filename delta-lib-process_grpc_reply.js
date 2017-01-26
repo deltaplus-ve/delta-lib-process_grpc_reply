25a6427aca4307a026c9a915d414ecef503fba66
Beto <albertoguerere@gmail.com>
2017-01-26 12:48:02 -0400

    Enable calling callback for triggering the reply event with the reply message on either the first or the second arguement of the callback

--- delta-lib-process_grpc_reply.js
+++ delta-lib-process_grpc_reply.js
@@ -15,19 +15,29 @@ var createMessage = function(code, message, data){
   };
 };
 
-var callCallbackWithReplyMessage = function(callback, replyMessage){
+var callCallbackWithReplyMessageOnFirstArgument = function(callback, replyMessage){
+  console.log(this.senderName + " - replyMessage: ", replyMessage);
+  callback(replyMessage);
+};
+
+var callCallbackWithReplyMessageOnSecondArgument = function(callback, replyMessage){
   console.log(this.senderName + " - replyMessage: ", replyMessage);
   callback(null, replyMessage);
 };
 
-var processReply = function(callback) {
+var processReply = function(callback, mode) {
   return function(error, data) {
     if (data && data.code && data.message && data.data && data.module){
       //Received data is in an envelope. We need to extract the data.
       data = data.data;
     }
-    if (!error) callCallbackWithReplyMessage(callback, createMessage(1, "Successful",
-      (typeof data === 'string' || data instanceof String) ? data : JSON.stringify(data)));
+    if (!error){
+      mode === 2 ?
+        callCallbackWithReplyMessageOnFirstArgument(callback,
+          createMessage(1, "Successful", (typeof data === 'string' || data instanceof String) ? data : JSON.stringify(data)))
+      : callCallbackWithReplyMessageOnSecondArgument(callback,
+          createMessage(1, "Successful", (typeof data === 'string' || data instanceof String) ? data : JSON.stringify(data)));
+    }
     else {
       console.log(this.senderName + " error: ", JSON.stringify(error));
       callCallbackWithReplyMessage(callback, createMessage(-1, "Error: " + error,
